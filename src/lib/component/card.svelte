980d84df9fff8f21db2a63876cd4c9308d084ced
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2023-09-21 12:41:09 +0200

    push

--- src/lib/component/card.svelte
+++ src/lib/component/card.svelte
@@ -5,11 +5,11 @@
     export let description = '';
 </script>
 
-<div class="mb-4 h-[600px] border-solid border-2 rounded-xl w-[90%] flex flex-col items-center hover:drop-shadow-lg transition-all hover:bg-[#F0F0F0] border-[#F0F0F0] hover:scale-105  duration-600">
+<div class="mb-4 h-[35rem] border-solid border-2 rounded-xl w-[90%] flex flex-col items-center hover:drop-shadow-lg transition-all hover:bg-[#F0F0F0] border-[#F0F0F0] hover:scale-105  duration-600">
     <div class="h-[50%] w-full ">
         <img src="{img}" class="object-cover w-full rounded-xl h-[100%]"/>
     </div>
-    <a href={href} class="px-8 h-[50%]">
+    <a href={href} class="px-8 h-[50%] flex flex-col items-center justify-center">
         <h5 class="text-center mt-3 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white font-roboto px-10 ">{title}</h5>
         <p class="text-center mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight font-roboto px-10">{description}</p>
     </a>
