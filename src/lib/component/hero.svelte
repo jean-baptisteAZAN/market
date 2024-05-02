593cf06148110e51c17e635815d0413bec98e3ee
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-05-02 12:31:09 +0200

    fix(engine): ambient light

--- src/lib/component/hero.svelte
+++ src/lib/component/hero.svelte
@@ -2,8 +2,7 @@
     let linkDonate = "https://donorbox.org/marketofflowers";
 </script>
 
-<div class="bg-cover bg-no-repeat bg-center  h-full flex flex-col items-center justify-center bg-[url('$lib/assets/hero2.jpg')]">
-
+<div class="bg-cover bg-no-repeat bg-center  h-full flex flex-col items-center justify-center bg-[url('/phtos/header.jpg')]">
     <div class="md:max-w-[100%] max-w-[80%] md:px-20 items-center justify-center md:-mt-[10%]">
         <div class="bg-green-800 bg-opacity-70 relative rounded-2xl flex flex-col justify-center items-center pt-10 pb-10 px-5">
             <h1 class="font-roboto text-center text-3xl uppercase text-white">
@@ -18,7 +17,7 @@
             </a>
         </div>
     </div>
-    <div class="absolute hidden md:flex  bottom-0 md:bottom-5 z-20 w-[80px] h-[80px] rounded-full border-solid border-4 animate-bounce justify-center items-center">
+    <div class="absolute hidden md:flex  bottom-0 md:bottom-5 z-20 rounded-full border-solid border-4 animate-bounce justify-center items-center">
         <img src="/arrow.png" alt="descendre" class="w-[60px]">
     </div>
 </div>
